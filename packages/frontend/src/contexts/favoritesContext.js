d035c59583b387de19bc523480d560eae9a6967c
FabriG <fabriciogatti7@gmail.com>
2024-03-08 09:19:07 -0300

    New Home Page

--- packages/frontend/src/contexts/favoritesContext.js
+++ packages/frontend/src/contexts/favoritesContext.js
@@ -1,5 +1,4 @@
 import React, { createContext, useContext, useState, useEffect } from 'react';
-
 const FavoritesContext = createContext();
 
 export const useFavorites = () => useContext(FavoritesContext);
